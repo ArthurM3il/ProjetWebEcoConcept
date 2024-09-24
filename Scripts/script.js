31a926ee14c501812a070d9960476a60b47eed31
ArthurM3il <morenoa@3il.fr>
2024-09-24 15:00:20 +0200

    Initial commit

---
+++ Scripts/script.js
@@ -0,0 +1,19 @@
+// Fonction pour inclure le header et le footer
+function includeHTML() {
+    // Chargement du header depuis le dossier Layout
+    fetch("Layouts/header.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("header-placeholder").innerHTML = data;
+      });
+  
+    // Chargement du footer depuis le dossier Layout
+    fetch("Layouts/footer.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("footer-placeholder").innerHTML = data;
+      });
+  }
+  
+  // Appel de la fonction pour charger le header et le footer
+  includeHTML();

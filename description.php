58cd18b408cfdda7d031eee2c8699015751c74c8
ArthurM3il <morenoa@3il.fr>
2024-10-25 11:05:04 +0200

    Ajout de la récupération des descriptions en BD

--- description.php
+++ description.php
@@ -1,24 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
 <?php
-require 'includes/bdd.php';
-require 'includes/header.php';
-require 'includes/menu.php';
+    require 'includes/bdd.php';
+    session_start();
 ?>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La terre des anges - Descriptions</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/polices.css">
+    <link rel="stylesheet" media="screen and (max-width: 768px)" href="css/mediaQueries.css">
+</head>
+<header><?php
+require_once 'includes/menu.php';
+require_once 'includes/function.php';
+$descriptions = getDescriptions($bdd);
+?>
+</header>
+<body>
 <h2>Descriptions</h2>
 
 <div id="descriptions">
     <?php
-    $descriptions = getDescriptions($bdd);
-
     foreach ($descriptions as $description): ?>
         <div class="description">
-            <img src="<?= $description['image'] ?>" alt="<?= $description['texte'] ?>">
-            <p><?= $description['texte'] ?></p>
+        <div class="description-content">
+        <h3><?php echo $description["titre"] ?></h3>
+         <p><?php echo $description["description"] ?></p>
+         <p><?php echo $description["prix"]?>€</p>
+        </div>
+            <img src="assets/imgs/<?php echo $description["image"] ?>" alt="<?php echo $description["alt"] ?>">
         </div>
     <?php endforeach; ?>
 </div>
+</body>
 
-
+<footer>
 <?php
 require 'includes/footer.php';
 ?>
+</footer>
--- includes/function.php
+++ includes/function.php
@@ -3,6 +3,12 @@ function getDescriptions($bdd) {
     if(!$bdd){
         die("Erreur de connexion à la BD".mysqli_connect_error());
     }
+    $sql = "SELECT id, titre, description, image, prix, alt FROM Description";
+
+    $stmt = $bdd->prepare($sql);
+    $stmt->execute();
+    $rows = $stmt->fetchAll();
+    return $rows; 
 }
 
 function getContact($bdd){
@@ -16,5 +22,18 @@ function getContact($bdd){
 
     $row = $stmt->fetchObject();
 
+    return $row; 
+}
+
+function getPresentation($bdd){
+    if (!$bdd) {
+        die("Erreur de connexion à la base de données : " . mysqli_connect_error());
+    }
+
+    $sql = "SELECT texte FROM Presentation LIMIT 1";
+    $stmt = $bdd->prepare($sql);
+    $stmt->execute();
+
+    $row = $stmt->fetchObject();
     return $row; 
 }
